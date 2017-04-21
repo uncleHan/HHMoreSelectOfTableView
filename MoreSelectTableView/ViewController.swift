74c544c18d453ec8a33cc70fcd71e9b9ee9cc0ea
heng han <heng.han@futuremove.cn>
2017-04-21 11:10:55 +0800

    Initial Commit

---
+++ MoreSelectTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoreSelectTableView
+//
+//  Created by 恒 韩 on 17/4/21.
+//  Copyright © 2017年 co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MoreSelectTableViewTests/MoreSelectTableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  MoreSelectTableViewTests.swift
+//  MoreSelectTableViewTests
+//
+//  Created by 恒 韩 on 17/4/21.
+//  Copyright © 2017年 co. All rights reserved.
+//
+
+import XCTest
+@testable import MoreSelectTableView
+
+class MoreSelectTableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
